eb042859e3fd66a8262c65c3eaa7e1a3b20b11ec
Filip Johnsson <filip.j.datatech@gmail.com>
2022-01-21 10:54:27 +0100

    Moved stuuf to Activity.jsx

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import Activity from './components/Activity';
 import './App.css';
-import { act } from 'react-dom/cjs/react-dom-test-utils.development';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [showActivty, setshowActivty] = useState(false);
+  const [startButton, setstartButton] = useState(false);
+
+
 
   return (
     <div className="App">
-    <h3>Inlämningsuppgift 2</h3>
-    <h1>Activity Application</h1>
-    <div className='content'>
-        <button className='button' onClick={() => {setshowActivty(!showActivty)
-        }}>{showActivty ? "Hide Activity": "Show Activity"}</button>  
-        
-    </div>
-    <div className='radio'>
-    <input type="radio" name='participants' value= "0" id="random"/>
-      <label htmlFor="random">Random participant(s)</label>
-      <input type="radio" name='participants' value= "1" id="part1"/>
-      <label htmlFor="part1">1 participant</label>
-      <input type="radio" name='participants' value= "2" id='part2' />
-      <label htmlFor="part2">2 participants</label>
-      <input type="radio" name='participants' value= "4" id='part4' />
-      <label htmlFor="part4">4 participants</label>
-    </div>
-      {showActivty && <Activity/>}
+      <h3>Inlämningsuppgift 2</h3>
+      <h1>Activity Application</h1>
+      <div>
+      
+       {!startButton ? (<button className='start' onClick={() => {setstartButton(true)
+        }}>Start Application</button>) 
+        : 
+        (<Activity/>)}
+      </div>
       <footer className='foot'><strong>Copyright Filip J</strong></footer>
     </div>
   );
--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
 import { useState, useEffect } from 'react';
 
 export default function Activity() {
 let radioButtons = document.querySelectorAll('input[name="participants"]');
     let selectedValue = 0;
     let url = "";
-
+    const [activities, setactivities] = useState({});
+    const [clicked, setclicked] = useState(1);
+    
     {radioButtons.forEach((button) => {
         if(button.checked){
             selectedValue = button.value;
@@ -13,6 +14,7 @@ let radioButtons = document.querySelectorAll('input[name="participants"]');
        return selectedValue;
     })}
     console.log(selectedValue);
+    console.log(clicked);
    
     (selectedValue != 0 ? url ="http://www.boredapi.com/api/activity?participants="+selectedValue: 
     url ="http://www.boredapi.com/api/activity/")
@@ -21,20 +23,34 @@ let radioButtons = document.querySelectorAll('input[name="participants"]');
         () =>{fetch(url)
         .then(response => response.json())
         .then(json => setactivities(json));
-    },[]
+    },[clicked]
         );
-    const [activities, setactivities] = useState({});
+
+    let handleClick = () => {
+        setclicked(clicked+1);
+    }
     
-
-      
-
-  return (
+return (
     <div> 
+        <h2>Choose how many participants or do not choose?</h2>
+        <div className='radio'>
+        <input type="radio" name='participants' value= "0" id="random"/>
+        <label htmlFor="random">Reset random participant(s)</label>
+        <input type="radio" name='participants' value= "1" id="part1"/>
+        <label htmlFor="part1">1 participant</label>
+        <input type="radio" name='participants' value= "2" id='part2' />
+        <label htmlFor="part2">2 participants</label>
+        <input type="radio" name='participants' value= "4" id='part4' />
+        <label htmlFor="part4">4 participants</label>
+        </div>
         <div className='act'>
+            <button className='button' onClick={handleClick}>New Activity</button>
             <h3><span>Activity:</span> {activities.activity}</h3>
             <h3><span>Type:</span> {activities.type}</h3>
             <h3><span>Participants:</span> {activities.participants}</h3>
         </div>
+
+        <h4> You have seen {clicked } {clicked > 1 ? "activities": "activity"} </h4>
     </div>
   );
 
